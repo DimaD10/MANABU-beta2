e7699fec7cb5f045e0868294180232863fb4fdd1
DimaD10 <94060040+DimaD10@users.noreply.github.com>
2022-11-04 21:26:06 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.retro__slider').slick({
+        zIndex: 1,
+        appendArrows: $('.retro__arrows'),
+    });
+    $('.emo__slider').slick({
+        zIndex: 1,
+        appendArrows: $('.emo__arrows'),
+    });
+});
